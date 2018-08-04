b64224a92419de8375501d66189895c51d5cba11
Sergiu <sil3ent@yahoo.com>
2018-08-04 18:22:30 +0300

    Add ShoppingList component & transitions

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import AppNavbar from './components/AppNavbar';
+import ShoppingList from './components/ShoppingList';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
@@ -8,6 +10,7 @@ class App extends Component {
     return (
       <div className="App">
         <AppNavbar />
+        <ShoppingList />
       </div>
     );
   }
